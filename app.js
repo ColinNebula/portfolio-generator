19854b88dc5aafc2b94d6f35c51d03aa76af184a
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-08-10 21:45:36 -0400

    added package.json,moment npm,inquirer npm

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;   
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+inquirer
+    .prompt([
+        { 
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+//run app by typing node app 
 
-console.log(name, github);
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+
+
+
+//Add moment 
+const moment = require('moment');
+const dateTime = moment();
+console.log(dateTime.format('MMMM Do YYYY, h:mm:ss a'));
