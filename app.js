ddf1e29e98262000cd32fbda44d2d2fcfaa15d1e
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-08-25 11:28:13 -0400

    updated app.js

--- app.js
+++ app.js
@@ -5,94 +5,18 @@ console.log(dateTime.format('MMMM Do YYYY, h:mm:ss a'));
 
 const inquirer = require('inquirer');
 
+const profileDataArgs = process.argv.slice(2);
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const profileDataArgs = process.argv.slice(2);
-
-// const [name, github] = profileDataArgs;
-// const pageHTML = generatePage(name, github);
-
-
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw(err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-const promptUser = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username'
-    },
-    {
-      type: 'input',
-      name: 'about',
-      message: 'Provide some information about yourself:'
-    }
-  ]);
-};
-//Projects Questions
-const promptProject = portfolioData => {
-  if (!portfolioData.projects) {
-    portfolioData.projects = [];
+const printProfileData = profileDataArr => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
   }
-  console.log(`
-=================
-Add a New Project
-=================
-`);
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is the name of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Provide a description of the project (Required)'
-    },
-    {
-      type: 'checkbox',
-      name: 'languages',
-      message: 'What did you build this project with? (Check all that apply)',
-      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
-    },
-    {
-      type: 'confirm',
-      name: 'feature',
-      message: 'Would you like to feature this project?',
-      default: false
-    },
-    {
-      type: 'confirm',
-      name: 'confirmAddProject',
-      message: 'Would you like to enter another project?',
-      default: false
-    }
-  ]);
+
+  console.log('================');
+
+  // Is the same as this...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-promptUser()
-  .then(answers => console.log(answers))
-  .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
-
-
-
-
-
+printProfileData(profileDataArgs);
