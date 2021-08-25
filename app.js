07eb4e8a1c5c2a45478da1f3024ed688f11ae928
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-08-25 15:35:28 -0400

    updated the index.html and page-template.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => { 
   return inquirer.prompt([
@@ -120,8 +120,9 @@ promptUser()
   .then(portfolioData => {
     console.log(portfolioData);
 // const profileDataArgs = process.argv.slice(2);
-// const pageHTML = generatePage(portfolioData);
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//   if (err) throw new Error(err);
-//   console.log('Portfolio complete! Check out index.html to see the output!');
+ const pageHTML = generatePage(portfolioData);
+ fs.writeFile('./index.html', pageHTML, err => {
+   if (err) throw new Error(err);
+   console.log('Page created! Check out index.html to see the output!');
+ });
 });
