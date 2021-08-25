b80e703f53e054558a41e8b121e68dc42be43ac0
ColinNebula <57843842+ColinNebula@users.noreply.github.com>
2021-08-25 13:59:28 -0400

    updated the app.js added questions

--- app.js
+++ app.js
@@ -1,55 +1,127 @@
-<<<<<<< HEAD
-//Moment
-const moment = require('moment');
-const dateTime = moment();
-console.log(dateTime.format('MMMM Do YYYY, h:mm:ss a'));
-
-const inquirer = require('inquirer');
-
-const profileDataArgs = process.argv.slice(2);
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
-=======
 const fs = require('fs');
+const inquirer = require('inquirer');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
+const promptUser = () => { 
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      // Validate the properties to check if a valid value was provided by the user
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => confirmAbout
+    },
+  ])
+}
+  const promptProject = portfolioData => {
+    
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+  // If there's no 'projects' array property, create one
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+}
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project? (Required)',
+        // Validate the properties to check if a valid value was provided by the user
+        validate: (value)=>{ if(value){return true} else {return 'we need a value here to continue please!'}},
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        // Validate the properties to check if a valid value was provided by the user
+        validate: (value)=>{ if(value){return true} else {return 'we need a value here to continue please!'}},
+        default: false
+      }
+    ])
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+// const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(portfolioData);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
 });
->>>>>>> generate-webpage
--- src/page-template.js
+++ src/page-template.js
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
-module.exports = (name, github) => {
-  return `
-  <!DOCTYPE html>
-  <html lang="en">
-  
-=======
 const generatePage = require('./src/page-template.js');
 const generatePage = (name, github) => {
   return `
   <!DOCTYPE html> 
   <html lang="en"> 
->>>>>>> generate-webpage
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
   </head>
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> generate-webpage
   <body>
     <h1>${name}</h1>
     <h2><a href="https://github.com/${github}">Github</a></h2>
@@ -29,7 +16,4 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
-<<<<<<< HEAD
-=======
 module.exports = generatePage;
->>>>>>> generate-webpage
